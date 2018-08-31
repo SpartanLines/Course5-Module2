5f0e2a5fed0cb491782f81e3a7a6bd94a06318ba
Youssef M. Nader <youssef.fattah96@eng-st.cu.edu.eg>
2018-09-01 01:10:24 +0200

    committed assignment

--- app.js
+++ app.js
@@ -1,24 +1,74 @@
-(function(){
-    'use strict';
-    angular.module('LunchCheck',[])
-        .controller('LunchCheckController',LunchCheckController)
-    LunchCheckController.inject= ['$scope'];
-    function LunchCheckController($scope){
-        $scope.message = "";
-        $scope.input="";
-        $scope.showMessage = function(){
-           var Toprocess = $scope.input.split(',');
-            if(Toprocess.length >3)
-            {
-                $scope.message="Too much!"
-            }
-            else if ($scope.input === "" )
-            {
-                $scope.message="Please Enter Data First"
-            }
-            else {
-                $scope.message="Enjoy"
-            }
-        };
-    }
+(function () {
+'use strict';
+
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('ShoppingListShowController', ShoppingListShowController)
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+  var ToBuy = this;
+
+  ToBuy.initialItems = [
+{ name: "cookies", quantity: 10 },
+{ name: "fries", quantity: 5 },
+{ name: "pizza", quantity: 11 },
+{ name: "salad", quantity: 14 },
+{ name: "veggies", quantity: 31 },
+{ name: "water", quantity: 7},
+{ name: "coke", quantity: 1 }
+  ];
+
+  ToBuy.addInitialItems = function () {
+    ShoppingListCheckOffService.addInitialItem(ToBuy.initialItems);
+  }
+  ToBuy.addInitialItems();
+  ToBuy.ToBuyItems=ShoppingListCheckOffService.getToBuyItems();
+  ToBuy.removeItem = function (itemIndex) {
+    ShoppingListCheckOffService.removeItem(itemIndex);
+  };
+}
+
+
+
+ShoppingListShowController.$inject = ['ShoppingListCheckOffService'];
+function ShoppingListShowController(ShoppingListCheckOffService) {
+  var CheckOff = this;
+
+  CheckOff.BoughtItems = ShoppingListCheckOffService.getBoughtItems();
+
+
+}
+
+
+function ShoppingListCheckOffService() {
+  var service = this;
+
+  // List of shopping items
+  var toBuy = [];
+  var bought= [];
+  service.addInitialItem = function (initialItems) {
+
+    console.log('I ve been invoked');
+  initialItems.forEach(function (item){
+    toBuy.push(item);
+  })
+
+  };
+
+  service.removeItem = function (itemIdex) {
+    var temp=toBuy.splice(itemIdex, 1);
+    console.log(temp);
+    bought.push(temp)
+  };
+
+  service.getToBuyItems = function () {
+    return toBuy;
+  };
+  service.getBoughtItems = function () {
+    return bought;
+  };
+}
+
 })();
